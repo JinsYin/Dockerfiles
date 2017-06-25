d61119607ebb76b83b69967d5ee1e81c39d18eb2
jinsyin <jinsyin@gmail.com>
2017-06-25 15:06:59 +0800

    modify hbase image

--- hbase/clear.sh
+++ hbase/clear.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Author: Jins Yin <jinsyin@gmail.com>
 
-rm -f ${HBASE_HOME}/*.txt ${HIVE_HOME}/LEGAL
+rm -f ${HBASE_HOME}/*.txt ${HBASE_HOME}/LEGAL
 rm -f ${HBASE_HOME}/conf/*.cmd
 rm -f ${HBASE_HOME}/bin/*.cmd
 rm -rf ${HBASE_HOME}/bin/test
